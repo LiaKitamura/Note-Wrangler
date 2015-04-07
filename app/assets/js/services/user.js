e50b3f7f483879b7521569c49098e906ac821a8f
LiaKitamura <kit4mura@gmail.com>
2015-04-07 00:05:59 -0600

    added resources for user

--- app/assets/js/services/user.js
+++ app/assets/js/services/user.js
@@ -1,9 +1,7 @@
-angular.module('NoteWrangler')
-  .factory('User', ['$resource',
-    function UserFactory($resource){
-      return $resource('/users/:id', { id: '@id'}, {
-        update: {
-          method: 'PUT'
-        }
-      });
-    }]);
+angular.module('NoteWrangler').factory('User', ['$resource', function UserFactory($resource){
+  return $resource('/users/:id', { id: '@id'}, {
+    update: {
+      method: 'PUT'
+    }
+  });
+}]);
