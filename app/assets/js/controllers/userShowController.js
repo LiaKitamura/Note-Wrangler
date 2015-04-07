065d83e39ba7dcf4c030027a9ac358c0f142eba9
LiaKitamura <kit4mura@gmail.com>
2015-04-07 00:04:53 -0600

    added userShowController and show page

--- app/assets/js/controllers/userShowController.js
+++ app/assets/js/controllers/userShowController.js
@@ -1,13 +1,9 @@
-angular.module('NoteWrangler')
-  .controller('UserShowController',
-    ['User', '$scope', '$routeParams', '$location',
-      function(User, $scope, $routeParams, $location){
+angular.module('NoteWrangler').controller('UserShowController', ['User', '$scope', '$routeParams', '$location', function(User, $scope, $routeParams, $location){
+  $scope.user = User.get({ id: $routeParams.id });
 
-        $scope.data = User.get({ id: $routeParams.id });
+  $scope.deleteUser = function(user){
+    User.remove({ id: user.id });
 
-        $scope.deleteUser = function(user){
-          User.remove({ id: user.id });
-
-          $location.path('/users/');
-        };
-      }]);
+    $location.path('/users/');
+  };
+}]);
